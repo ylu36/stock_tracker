9b46b1c80b741480ab6ce1b31a2664c97cd735b4
Yuanchen Lu <Yuanchen.Lu@ibm.com>
2020-04-20 21:46:55 -0400

    can render friends

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
+  get 'my_friends', to: 'users#my_friends'
+  resources :friendships, only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
